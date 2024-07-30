996c034a1a2d09a09a3e265d671d13fb5e099c31
Tahirsultan777 <mtahirsultanmtahir@gmail.com>
2024-07-30 06:02:32 -0700

    task4

---
+++ Task 4/Frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,77 @@
+import { useNavigate } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+import "./Cart.css";
+import React, { useContext } from "react";
+
+const Cart = () => {
+  const { CartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+   const navigate = useNavigate();
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (CartItems[item._id] > 0) {
+            return (
+              <div className="">
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>RS_{item.price}</p>
+                  <p>{CartItems[item._id]}</p>
+                  <p>RS_{item.price * CartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    X
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-detail">
+              <p>Subtotal</p>
+              <p>RS_{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <p>Delivery Fee</p>
+              <p>RS_{getTotalCartAmount()===0?0:50}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <b>Total</b>
+              <b>RS_{getTotalCartAmount()===0?0:getTotalCartAmount()+50}</b>
+            </div>
+          </div>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code,Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
